517edd566a48ddba34b7199645880bd4d2015db4
adimaniac <adityabhigade31@gmail.com>
2024-10-24 02:57:32 +0530

    color scheme selected

--- SolvathonFrontend/tailwind.config.js
+++ SolvathonFrontend/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#C7FFD8', // Lightest primary color
+          DEFAULT: '#98DED9', // Default primary color
+          dark: '#0B2F9F', // Darker primary color
+          darkest: '#161D6F', // Darkest primary color
+        },
+      },
+    },
   },
   plugins: [],
 }
